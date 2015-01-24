2cba29d5e51394a194fc5abb35cfdd5218e04641
tuanlethanhtn@gmail.com <tuanlethanhtn@gmail.com@cb09f6b3-ffbe-8685-162f-0428a700b215>
2015-01-24 15:20:28 +0000

    Add ProductManagement Page

---
+++ SourceCode/SBPC System/SBPC System/Controllers/ProductManagementController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBPC_System.Controllers
+{
+    public class ProductManagementController : Controller
+    {
+        //
+        // GET: /ProductManagement/
+
+        public ActionResult ProductManagement()
+        {
+            return View();
+        }
+        public ActionResult RedirectToAddProduct()
+        {
+            return Redirect("AddProduct");
+
+        }
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        
+    }
+}
