15602d0ab3f06cbbe01ed447be066a6aff48f23e
tuanlethanhtn@gmail.com <tuanlethanhtn@gmail.com@cb09f6b3-ffbe-8685-162f-0428a700b215>
2015-01-25 16:14:48 +0000

    Add mapping page (popup in builder Page), update edit and add Product Page

---
+++ trunk/SourceCode/SBPC System/SBPC System/Controllers/MapDomainController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBPC_System.Controllers
+{
+    public class MapDomainController : Controller
+    {
+        //
+        // GET: /MapDomain/
+
+        public ActionResult MapDomain()
+        {
+            return View();
+        }
+
+    }
+}
