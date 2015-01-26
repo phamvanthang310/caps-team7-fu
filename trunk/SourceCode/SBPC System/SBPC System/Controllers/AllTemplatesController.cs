7c439d5b80cd3fec9da77e929d2acd4aab0843e1
tuanlethanhtn@gmail.com <tuanlethanhtn@gmail.com@cb09f6b3-ffbe-8685-162f-0428a700b215>
2015-01-26 09:03:36 +0000

    Add View templates page

---
+++ trunk/SourceCode/SBPC System/SBPC System/Controllers/AllTemplatesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBPC_System.Controllers
+{
+    public class AllTemplatesController : Controller
+    {
+        //
+        // GET: /AllTemplates/
+
+        public ActionResult AllTemplates()
+        {
+            return View();
+        }
+
+    }
+}
