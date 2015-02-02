2fae58f561c41e06f08ce56c4bb561c0cc61db05
thinhlt789520@gmail.com <thinhlt789520@gmail.com@cb09f6b3-ffbe-8685-162f-0428a700b215>
2015-02-02 14:09:36 +0000

    Sub project for login and send mail

---
+++ SourceCode/SBPC System/TemptProject/Areas/User/Controllers/ProfileController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TemptProject.Areas.User.Controllers
+{
+    public class ProfileController : Controller
+    {
+        //
+        // GET: /User/Profile/
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult Upgrade()
+        {
+            return View();
+        }
+
+        public ActionResult SiteManagement()
+        {
+            return View();
+        }
+
+        public ActionResult Statistic()
+        {
+            return View();
+        }
+    }
+}
