a9c827a216710743eb647c3501bd1b660d11d652
tuanlethanhtn@gmail.com <tuanlethanhtn@gmail.com@cb09f6b3-ffbe-8685-162f-0428a700b215>
2015-02-04 15:11:20 +0000

    Add CategoryManagement Page for Admin

---
+++ trunk/SourceCode/SBPC System/SBPC System/Controllers/CategoryManagementController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBPC_System.Controllers
+{
+    public class CategoryManagementController : Controller
+    {
+        //
+        // GET: /CategoryManagement/
+
+        public ActionResult CategoryManagement()
+        {
+            return View();
+        }
+
+        public ActionResult RedirectToAddCategory()
+        {
+            return Redirect("AddCategory");
+        }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+    }
+}
